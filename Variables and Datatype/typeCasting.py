ada60a51a6ed33b64c18c07fa5f04fca516d8e8a
Ali Hasan <77145109+AliHaasan-Scientist@users.noreply.github.com>
2021-11-21 20:40:03 +0500

    Add files via upload

---
+++ Variables and Datatype/typeCasting.py
@@ -0,0 +1,13 @@
+#string to other data type
+string="123";
+intCasting=int(string);
+print(type(intCasting));
+#Numbers casting
+integar=124;
+floatCasting=float(integar);
+print(floatCasting)
+stringCasting=str(integar)
+print(type(stringCasting))
+floatNumber=13.2885;
+intCasting=int(floatNumber);
+print(intCasting);
