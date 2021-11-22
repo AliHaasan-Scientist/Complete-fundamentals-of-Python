823ad6c6be0df364dca4b1caa5871434ecd47ed8
Ali Hasan <77145109+AliHasan-Scientist@users.noreply.github.com>
2021-11-22 14:01:39 +0500

    Delete Variables and Datatype directory

--- Variables and Datatype/04_userInput.py
+++
@@ -1,7 +0,0 @@
-# input in python (Input function take an iput as a string by default)
-target = input("Enter anythin from keyboard");
-print(target);
-# we can convert the type of input using type casting
-number = input("Enter a number");
-print(type(number));
-print(type(int(number)));
